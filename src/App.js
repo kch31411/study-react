e1b71235a50f98c5c90ad98051f8187834be965a
이혜승 <30349090+huusz@users.noreply.github.com>
2020-07-13 11:11:07 +0900

    연습용 코드를 업로드할 페이지 분할

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CH from "./ch";
+import EY from "./ey";
+import GW from "./gw";
+import HS from "./hs";
+import SY from "./sy";
+
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/ch">CH</Link>
+            </li>
+            <li>
+              <Link to="/ey">EY</Link>
+            </li>
+            <li>
+              <Link to="/gw">GW</Link>
+            </li>
+            <li>
+              <Link to="/hs">HS</Link>
+            </li>
+            <li>
+              <Link to="/sy">SY</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/ch">
+            <CH />
+          </Route>
+          <Route path="/ey">
+            <EY />
+          </Route>
+          <Route path="/gw">
+            <GW />
+          </Route>
+          <Route path="/hs">
+            <HS />
+          </Route>
+          <Route path="/sy">
+            <SY />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
