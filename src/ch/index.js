5bacb9681f65e1f1d47d8a94e0bef57d27dce8c0
김창하 <kch31411@kaist.ac.kr>
2020-07-21 00:37:57 +0900

    useState

--- src/ch/index.js
+++ src/ch/index.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function FunctionComponent({ color, value, showBtn }) {
+  const [number, setNumber] = useState(Number(value));
+
+  const onIncrease = () => {
+    setNumber(number + 1);
+  };
+  const onDecrease = () => {
+    setNumber(prev => prev - 1);
+  };
+
   return (
     <>
       <h2 className="number" style={{ color: color }}>
-        {value}
+        {number}
       </h2>
       {showBtn && (
         <div>
-          <button id="increase">+1</button>
-          <button id="decrease">-1</button>
+          <button onClick={onIncrease} id="increase">
+            +1
+          </button>
+          <button onClick={onDecrease} id="decrease">
+            -1
+          </button>
         </div>
       )}
     </>
